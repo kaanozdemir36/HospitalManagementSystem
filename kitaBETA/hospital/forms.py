51da0c8d33e8307f18312419b6f5c4304169a953
Kaan Özdemir <47723053+kaanozdemir36@users.noreply.github.com>
2021-01-10 21:29:22 +0300

    Add files via upload

---
+++ kitaBETA/hospital/forms.py
@@ -0,0 +1,69 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+
+class AdminSigupForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+
+
+
+class DoctorUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+class DoctorForm(forms.ModelForm):
+    class Meta:
+        model=models.Doctor
+        fields=['address','mobile','department','status','profile_pic']
+
+
+
+
+class PatientUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+class PatientForm(forms.ModelForm):
+
+    class Meta:
+        model=models.Patient
+        fields=['address','mobile','status','symptoms','profile_pic']
+
+
+
+class AppointmentForm(forms.ModelForm):
+    doctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Doctor Name and Department", to_field_name="user_id")
+    patientId=forms.ModelChoiceField(queryset=models.Patient.objects.all().filter(status=True),empty_label="Patient Name and Symptoms", to_field_name="user_id")
+    class Meta:
+        model=models.Appointment
+        fields=['description','status']
+
+
+class PatientAppointmentForm(forms.ModelForm):
+    doctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Doctor Name and Department", to_field_name="user_id")
+    class Meta:
+        model=models.Appointment
+        fields=['description','status']
+
+
+#CONTACT US
+class ContactusForm(forms.Form):
+
+    Name = forms.CharField(label='‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎‎‎‎‎‎ ‎‎‎‎ ‎‎‎‎ ‎‎‎‎ ‎‎‎‎  Name',max_length=30)
+    Username = forms.CharField(label='‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎  Username ',max_length=30)
+    Email = forms.EmailField(label='‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎‎‎‎‎‎ ‎‎‎‎ ‎‎‎‎ ‎‎‎‎ ‎‎‎‎ Email')
+    Message = forms.CharField(label='Message (Optional) ',max_length=300,widget=forms.Textarea(attrs={'rows': 3, 'cols': 24}))
+
+
